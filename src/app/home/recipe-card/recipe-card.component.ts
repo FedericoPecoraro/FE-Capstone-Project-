10fa2cbe1a186395a9b9fe59c7ba7e65f805325c
FedericoPecoraro <federico.pecoraro8@gmail.com>
2024-07-08 02:47:11 +0200

    logo, favicon and hro responsivness added

--- src/app/home/recipe-card/recipe-card.component.ts
+++ src/app/home/recipe-card/recipe-card.component.ts
@@ -23,41 +23,42 @@ export class RecipeCardComponent implements OnInit {
     });
   }
 
-  addToFavorites(recipe: RecipeResponse) {
+  toggleFavorite(recipe: RecipeResponse) {
     const userId = this.userSvc.getCurrentUserId();
     if (userId) {
-      this.userSvc.likeRecipe(userId, recipe.id).subscribe(
-        () => {
-          console.log(`Recipe ${recipe.id} added to favorites.`);
-          // Aggiungi logica aggiuntiva se necessario dopo l'aggiunta ai preferiti
-        },
-        error => {
-          console.error('Error adding recipe to favorites:', error);
-          // Gestione degli errori
-        }
-      );
+      if (this.isFavorite(recipe)) {
+        this.userSvc.unlikeRecipe(userId, recipe.id).subscribe(
+          () => {
+            console.log(`Recipe ${recipe.id} removed from favorites.`);
+            // Aggiungi logica aggiuntiva se necessario dopo la rimozione dai preferiti
+          },
+          error => {
+            console.error('Error removing recipe from favorites:', error);
+            // Gestione degli errori
+          }
+        );
+      } else {
+        this.userSvc.likeRecipe(userId, recipe.id).subscribe(
+          () => {
+            console.log(`Recipe ${recipe.id} added to favorites.`);
+            // Aggiungi logica aggiuntiva se necessario dopo l'aggiunta ai preferiti
+          },
+          error => {
+            console.error('Error adding recipe to favorites:', error);
+            // Gestione degli errori
+          }
+        );
+      }
     } else {
       console.error('User not authenticated or token invalid.');
       // Gestione caso in cui l'utente non è autenticato o il token non è valido
     }
   }
 
-  removeFromFavorites(recipe: RecipeResponse) {
-    const userId = this.userSvc.getCurrentUserId();
-    if (userId) {
-      this.userSvc.unlikeRecipe(userId, recipe.id).subscribe(
-        () => {
-          console.log(`Recipe ${recipe.id} removed from favorites.`);
-          // Aggiungi logica aggiuntiva se necessario dopo la rimozione dai preferiti
-        },
-        error => {
-          console.error('Error removing recipe from favorites:', error);
-          // Gestione degli errori
-        }
-      );
-    } else {
-      console.error('User not authenticated or token invalid.');
-      // Gestione caso in cui l'utente non è autenticato o il token non è valido
-    }
+  isFavorite(recipe: RecipeResponse): boolean {
+    // Implementa la logica per verificare se la ricetta è nei preferiti dell'utente
+    // Potresti usare una logica basata su userId e recipe.id
+    // Ad esempio, controllando se recipe.id è presente nella lista dei preferiti dell'utente corrente
+    return false; // Sostituisci con la tua logica reale
   }
 }
